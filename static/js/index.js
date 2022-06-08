0bc6fabbcc2ceb49f5aa63c1efac11183a424715
damonyuhan <damonyuhan@tencent.com>
2022-06-08 20:46:17 +0800

    修改主页

---
+++ static/js/index.js
@@ -0,0 +1,137 @@
+var xhr = new XMLHttpRequest();
+xhr.open( 'get', 'https://v1.hitokoto.cn?c=i' );
+xhr.onreadystatechange = function () {
+  if ( xhr.readyState === 4 ) {
+    var data = JSON.parse( xhr.responseText );
+    var hitokoto = document.getElementById( 'hitokoto' );
+    hitokoto.innerHTML = "";
+    //hitokoto.appendChild(document.createTextNode(data.hitokoto))
+    var typed = new Typed( "#hitokoto", {
+      strings: [ data.hitokoto ],
+      typeSpeed: 80
+    } );
+  }
+}
+xhr.send();
+
+var plaaaay = function ( id ) {
+  //$('#' + id)[0].play();
+}
+var exWords = function ( id, str ) {
+  $( id )
+    .html( str );
+}
+var swLinks = function ( st ) {
+  if ( st == 1 ) {
+    $( '#link' )
+      .removeClass( 'l-hidden' );
+    $( '#l-back' )
+      .removeClass( 'l-hidden' );
+    plaaaay( 'ruready' );
+  } else if ( st == 0 ) {
+    $( '#link' )
+      .addClass( 'l-hidden' );
+    $( '#l-back' )
+      .addClass( 'l-hidden' );
+  }
+}
+var getLinks = function ( url ) {
+  $.getJSON( url, {
+      t: Date.parse( new Date() )
+    },
+    function ( result ) {
+      $.each( result,
+        function ( i, obj ) {
+          $.each( obj,
+            function ( n, links ) {
+              var l_group = $( '<div class="l-group"></div>' );
+              var l_title = $( '<div class="l-title"><h1>' + n + '</h1></div>' );
+              l_group.append( l_title );
+              var l_box = $( '<div class="l-box"></div>' );
+              //links.sort(function(){ return 0.5 - Math.random() }); // random
+              $.each( links,
+                function ( j, link ) {
+                  var lil_url = link.logo != null ? link.logo : './src/loading.svg';
+                  var lid_text = link.desc != null ? link.desc : '暂无描述 ┑(￣Д ￣)┍';
+                  var l_item = $( '<div class="l-item"></div>' );
+                  l_item.attr( "title", '✦ ' + link.name + '\n✧ ' + lid_text + '\n✧ ' + link.href );
+                  l_item.bind( 'click',
+                      function () {
+                        window.open( link.href + ( link.noUtm === undefined ? '' : '' ) );
+                      } )
+                    .bind( "contextmenu",
+                      function () {
+                        showQr( link.href + ( link.noUtm === undefined ? '?qrCode=hanyu.cool' : '' ), link.name + '<br><span>' + lid_text + '</span>', lil_url );
+                        return false;
+                      } );
+                  var li_logo = $( '<div class="li-logo"></div>' );
+                  li_logo.css( 'background-image', 'url(' + lil_url + ')' );
+                  l_item.append( li_logo );
+                  var li_name = $( '<h2>' + link.name + '</h2>' );
+                  l_item.append( li_name );
+                  var li_desc = $( '<span>' + lid_text + '</span>' );
+                  l_item.append( li_desc );
+                  l_item.bind( 'mouseover',
+                    function () {
+                      $( '.l-back' )
+                        .css( 'background-image', 'url(' + lil_url + ')' );
+                    } );
+                  l_box.append( l_item );
+                } );
+              l_group.append( l_box );
+              $( '#link' )
+                .append( l_group );
+            } );
+        } );
+    } );
+}
+var randomNum = function ( minNum, maxNum ) {
+  switch ( arguments.length ) {
+    case 1:
+      return parseInt( Math.random() * minNum + 1, 10 );
+      break;
+    case 2:
+      return parseInt( Math.random() * ( maxNum - minNum + 1 ) + minNum, 10 );
+      break;
+    default:
+      return 0;
+      break;
+  }
+}
+var showQr = function ( url, txt, logo ) {
+  var qrnode = new AraleQRCode( {
+    render: 'canvas',
+    correctLevel: 2,
+    text: url,
+    size: 260,
+    background: '#fff',
+    foreground: '#000',
+    pdground: '#f33',
+    image: logo,
+    imageSize: 32
+  } );
+  $( '#qrImg' )
+    .html( qrnode );
+  $( '#qrCode' )
+    .css( 'display', 'block' );
+  $( '#qrDesc' )
+    .html( txt );
+}
+var hideQr = function ( url ) {
+  $( '#qrCode' )
+    .css( 'display', 'none' );
+}
+var duoMaomao = function () {
+  var maomao = $( '#maomao' );
+  maomao.css( 'bottom', randomNum( 5, 80 ) + 'vh' );
+}
+$( function () {
+  getLinks('./static/config/links.json');
+  //getLinks( 'https://file.hanyu.cool/site/home/json/clear/links.json' );
+  if ( location.hash == '#link' ) {
+    $( '#link' )
+      .removeClass( 'l-hidden' );
+    $( '#l-back' )
+      .removeClass( 'l-hidden' );
+  }
+} );
